ad4b5cb4c6034df14f23b7c6b2257fb99ee97891
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-03-11 14:24:54 +0000

    updated modal to contain the iframe

--- customScript.js
+++ customScript.js
@@ -1,15 +1,31 @@
 $(document).ready(function() {
-    // Trailer button functionality
-    $('.trailer-btn').click(function() {
-      var trailerUrl = $(this).data('trailer-url');
-      var iframeHtml = '<iframe width="560" height="315" src="' + trailerUrl + '?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-      $('.bd-example-modal-lg .modal-body').empty().append(iframeHtml);
-    });
-  
-    // Clear the iframe when the modal is hidden
-    $('.bd-example-modal-lg').on('hidden.bs.modal', function () {
-      $(this).find('.modal-body').empty();
-    });
+// Trailer button functionality
+$('.trailer-btn').click(function() {
+  var trailerUrl = $(this).data('trailer-url');
+  var iframeWidth = 560; // iframe width
+  var iframeHeight = 315; // iframe height
+  var modalPadding = 30; // Adjust this value based on your modal's padding and desired spacing
+
+  var iframeHtml = '<iframe width="' + iframeWidth + '" height="' + iframeHeight + '" src="' + trailerUrl + '?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+  $('.bd-example-modal-lg .modal-body').empty().append(iframeHtml);
+
+  // Adjust modal dialog size dynamically
+  $('.bd-example-modal-lg .modal-dialog').css({
+    'max-width': (iframeWidth + modalPadding) + 'px' // Add some extra width for padding/borders
+  });
+
+  // Optionally, you can adjust the height of the modal content if necessary
+  $('.bd-example-modal-lg .modal-content').css({
+    'height': 'auto', // Set to auto to adjust height based on the content
+    'min-height': (iframeHeight + modalPadding) + 'px' // Ensure the modal is tall enough
+  });
+});
+
+// Clear the iframe when the modal is hidden
+$('.bd-example-modal-lg').on('hidden.bs.modal', function () {
+  $(this).find('.modal-body').empty();
+});
   
     //Genre Filter functionality
     $('.genre-filter').click(function(e) {
